3debc4a7ffee73f1a98670b4722f468cb09b8054
hongweimachene <hchen00@stuy.edu>
2018-11-24 17:39:06 -0500

    added compareTo

--- MyString.java
+++ MyString.java
@@ -1,4 +1,4 @@
-public class MyString implements CharSequence {
+public class MyString implements CharSequence, Comparable<CharSequence> {
   private char[] data;
 
   //constructor takes CharSequence and puts it in a char array
@@ -9,7 +9,6 @@ public class MyString implements CharSequence {
     }
   }
 
-  //
   public char charAt(int index) {
     //if index isnt valid throw an exception
     if (index >= data.length) {
@@ -22,11 +21,12 @@ public class MyString implements CharSequence {
     return data.length;
   }
 
-  //returns a string as a CharSequence by adding from start up to but not including end
   public CharSequence subSequence(int start, int end) {
+    //if start or end isnt valid throw an exception
     if (start >= data.length || end > data.length || start < 0 || end < 0) {
       throw new IndexOutOfBoundsException();
     }
+    //makes a string and adds values of data from index start up to but not including end
     String s = "";
     for (int i = start; i < end; i++) {
       s+=data[i];
@@ -42,10 +42,32 @@ public class MyString implements CharSequence {
     return s;
   }
 
-  public static void main(String[] args) {
+  public int compareTo(CharSequence o) {
+    if (o == null) {
+      throw new NullPointerException();
+    }
+    if (length() < o.length()) {
+      return -1;
+    }
+    if (length() > o.length()) {
+      return 1;
+    }
+    for (int i = 0; i < length(); i++) {
+      if (charAt(i) < o.charAt(i)) {
+        return -1;
+      }
+      if (charAt(i) > o.charAt(i)) {
+        return 1;
+      }
+    }
+    return 0;
+  }
+
+  /*public static void main(String[] args) {
     MyString z = new MyString("abc");
     MyString y = new MyString("hllelo");
     MyString x = new MyString("yaayeet");
+    MyString a = new MyString("zaayeet");
     System.out.println(z.length() +  " length ");
     System.out.println(x.length() + " length ");
     System.out.println(y.length() + " length");
@@ -54,5 +76,8 @@ public class MyString implements CharSequence {
     System.out.println(y.charAt(4));
     System.out.println(x.charAt(3));
     System.out.println(x.subSequence(0,4));
-    }
+    System.out.println(z.compareTo(x));
+    System.out.println(x.compareTo(a));
+    System.out.println(a.compareTo(x));
+  }*/
 }
